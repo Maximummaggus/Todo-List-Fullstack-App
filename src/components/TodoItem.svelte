47528c8ed970df7838a32e1c73f5d8cb17c7c50a
Simon <simon.chhit@edu.teko.ch>
2024-08-25 21:15:51 +0200

    [New] adding editor function for todos

--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -4,16 +4,38 @@
     export let todo;
 
     const dispatch = createEventDispatcher();
+    let editMode = false;
+    let editedText = todo.text;
 
     function handleRemove() {
         dispatch('removeTodo', todo.id);
     }
+
+    function handleEdit() {
+        editMode = true;
+    }
+
+    function saveEdit() {
+        dispatch('editTodo', { id: todo.id, text: editedText });
+        editMode = false;
+    }
+
+    function cancelEdit() {
+        editMode = false;
+        editedText = todo.text; // Reset edited text to original
+    }
 </script>
 
 <li>
-    {todo.text}
-    <button on:click={handleRemove}>Remove</button>
-    <dropdown/>
+    {#if editMode}
+        <input bind:value={editedText} on:keydown={(e) => e.key === 'Enter' && saveEdit()} />
+        <button on:click={saveEdit}>Save</button>
+        <button on:click={cancelEdit}>Cancel</button>
+    {:else}
+        {todo.text}
+        <button on:click={handleEdit}>Edit</button>
+        <button on:click={handleRemove}>Remove</button>
+    {/if}
 </li>
 
 <style>
@@ -32,5 +54,11 @@
         padding: 4px 8px;
         border-radius: 4px;
         cursor: pointer;
+        margin-left: 5px;
+    }
+
+    input {
+        flex: 1;
+        margin-right: 5px;
     }
 </style>
